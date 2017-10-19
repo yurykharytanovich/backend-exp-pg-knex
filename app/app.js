b83538873f682a55848e6bfb710f62a3a173c70d
YuryKharytanovich <yurykharytanovich@gmail.com>
2017-10-19 20:05:31 +0300

    fix 22

--- app/app.js
+++ app/app.js
@@ -1,29 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import cors from 'cors';
-// import morgan from 'morgan';
+import cors from 'cors';
+import morgan from 'morgan';
 import api from './api';
 
-// const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 const app = express();
 
-// if (env === 'production') {
-//     app.use(function (req, res, next) {
-//         if (req.headers['x-forwarded-proto'] !== 'https') {
-//             return res.redirect(['https://', req.get('Host'), req.url].join(''));
-//         }
-//
-//         return next();
-//     });
-// }
+if (env === 'production') {
+    app.use(function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(['https://', req.get('Host'), req.url].join(''));
+        }
 
-// app.use(cors());
+        return next();
+    });
+}
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// if(app.settings.env === 'development') {
-//     app.use(morgan('dev'));
-// }
+if(app.settings.env === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.get('/', function (req, res) {
     res.send('Backend-prototype API server ready...');
