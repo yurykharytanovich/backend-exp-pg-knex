5b9cf80b1624f57901d0c6bba0d3369403f90304
YuryKharytanovich <yurykharytanovich@gmail.com>
2017-10-20 00:10:59 +0300

    fix 25

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     development: {
         client: 'postgresql',
-        connection: {
+        connection: process.env.DATABASE_URL || {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
@@ -13,37 +13,5 @@ module.exports = {
             tableName: process.env.DB_MIGR,
         },
 
-    },
-
-    staging: {
-        client: 'postgresql',
-        connection: {
-            user: process.env.DB_USER,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASS,
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            tableName: process.env.DB_MIGR,
-        },
-    },
-
-    production: {
-        client: 'postgresql',
-        connection: {
-            user: process.env.DB_USER,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASS,
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            tableName: process.env.DB_MIGR,
-        },
     },
 };
