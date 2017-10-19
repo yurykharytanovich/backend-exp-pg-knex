7fd45c450febf3db91d9db9e299aab52db619a02
YuryKharytanovich <yurykharytanovich@gmail.com>
2017-10-19 18:52:32 +0300

    fix 14

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import morgan from 'morgan';
-import api from './api';
+// import bodyParser from 'body-parser';
+// import cors from 'cors';
+// import morgan from 'morgan';
+// import api from './api';
 
-const env = process.env.NODE_ENV || 'development';
+// const env = process.env.NODE_ENV || 'development';
 const app = express();
 
 // if (env === 'production') {
@@ -16,28 +16,28 @@ const app = express();
 //         return next();
 //     });
 // }
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-if(app.settings.env === 'development') {
-    app.use(morgan('dev'));
-}
+//
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+//
+// if(app.settings.env === 'development') {
+//     app.use(morgan('dev'));
+// }
 
 app.get('/', function (req, res) {
     res.send('Backend-prototype API server ready...');
 });
 
-app.use('/', api);
-
-app.use((err, req, res, next) => {
-    if (err) {
-        console.error(err.name + ': ' + err.message);
-        res.status(500).send(err.message);
-    } else {
-        next(req, res);
-    }
-});
+// app.use('/', api);
+//
+// app.use((err, req, res, next) => {
+//     if (err) {
+//         console.error(err.name + ': ' + err.message);
+//         res.status(500).send(err.message);
+//     } else {
+//         next(req, res);
+//     }
+// });
 
 export default app;
